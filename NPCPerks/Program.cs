e2490b76bb68f43a18477bbd9618ee2e46fad082
Vitor Daniel <vitor.daniel@outlook.pt>
2019-03-22 10:25:45 +0000

    Add project files.

---
+++ NPCPerks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NPCPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
